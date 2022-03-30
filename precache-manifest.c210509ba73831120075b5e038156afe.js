2f1b06a1f04e0182e103bf30831abf85ad05d6e1
Chris Marquez <ixdchrismarquez@gmail.com>
2022-03-30 15:52:28 -0700

    Updates

---
+++ precache-manifest.c210509ba73831120075b5e038156afe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e65908753f7baf5dc7ed",
+    "url": "/robofriends/static/css/main.ce87f0c8.chunk.css"
+  },
+  {
+    "revision": "e65908753f7baf5dc7ed",
+    "url": "/robofriends/static/js/main.4b83033d.chunk.js"
+  },
+  {
+    "revision": "c85c82f5ba82327a3218",
+    "url": "/robofriends/static/js/runtime~main.f7dcbb18.js"
+  },
+  {
+    "revision": "8231f7e8cc1ae92e746f",
+    "url": "/robofriends/static/css/2.ddbdd015.chunk.css"
+  },
+  {
+    "revision": "8231f7e8cc1ae92e746f",
+    "url": "/robofriends/static/js/2.110e8b23.chunk.js"
+  },
+  {
+    "revision": "9c19088db84563606874",
+    "url": "/robofriends/static/js/3.5526bd81.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "48b4cd502d2e497d13d7b04e493ab432",
+    "url": "/robofriends/index.html"
+  }
+];
